d1d81edb122bfca8b126258098cdce637d4db2b7
Himanshu Chandola <himanshuchandolaofficial@gmail.com>
2021-08-17 19:53:38 +0530

    word and character counter added

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -14,12 +14,18 @@ export default function TextForm(props) {
     }
     const[text, setText] = useState('Enter Text Here');
     return (
-        <div>
+        <>
+        <div ClassName="container">
             <h1>{props.heading}</h1>
             <div className="mb-3">
             <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
                 </div> 
                 <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
         </div>
+        <div className="container my-3">
+            <h1>Your Text Summary</h1>
+            <p>{text.split(" ").length} words and {text.length} characters</p>
+        </div>
+        </>
     )
 }
