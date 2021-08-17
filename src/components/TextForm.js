9daf01cf479f4eebb45d649d3adc0f0ec99a5c1e
Himanshu Chandola <himanshuchandolaofficial@gmail.com>
2021-08-17 20:03:24 +0530

    Reading time counter added, 125 words Per minute is set up here or 0.008 minutes to read 1 word

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -25,6 +25,7 @@ export default function TextForm(props) {
         <div className="container my-3">
             <h1>Your Text Summary</h1>
             <p>{text.split(" ").length} words and {text.length} characters</p>
+            <p>{0.008 * text.split(" ").length} Minutes to Read the above Text</p>
         </div>
         </>
     )
