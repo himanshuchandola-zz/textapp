fb895ee9960e6f243d63990f311d57ebf15a6289
Himanshu Chandola <himanshuchandolaofficial@gmail.com>
2021-08-17 20:10:17 +0530

    Added Preview Text and Lowercase Functionality

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,6 +7,12 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         setText(newText)
     }
+    const handleLoClick = ()=>{
+        // console.log("Uppercase was Clicked" + text);
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
+
 
     const handleOnChange = (event)=>{
        // console.log("on Change");
@@ -20,12 +26,16 @@ export default function TextForm(props) {
             <div className="mb-3">
             <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
                 </div> 
-                <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
+                <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
+                <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to LowerCase</button>
+
         </div>
         <div className="container my-3">
-            <h1>Your Text Summary</h1>
+            <h1>Text Summary</h1>
             <p>{text.split(" ").length} words and {text.length} characters</p>
             <p>{0.008 * text.split(" ").length} Minutes to Read the above Text</p>
+            <h2>Preview Text</h2>
+            <p>{text}</p>
         </div>
         </>
     )
