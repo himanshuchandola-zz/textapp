fcf3ec2f1b8011f1e35794bcd8d5846c1542b182
Himanshu Chandola <himanshuchandolaofficial@gmail.com>
2021-08-17 18:49:23 +0530

    Added TextForm Component for TextArea

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 function App() {
   return (
   <>
 <Navbar title="TextApp" aboutText="About"/>
+<div className="container my-3">
+<TextForm heading="Enter The Text to analyze"/>
+</div>
+
   </>
   );
 }
