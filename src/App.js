47892dbcfc9b41d8b54a809a0c6031b7288e7a85
Himanshu Chandola <himanshuchandolaofficial@gmail.com>
2021-08-17 18:12:36 +0530

    adding title as prop in navbar

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar';
 function App() {
   return (
   <>
-<Navbar/>
+<Navbar title="TextApp"/>
   </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function Navbar() {
+export default function Navbar(props) {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">Textapp</a>
+          <a className="navbar-brand" href="/">{props.title}</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
