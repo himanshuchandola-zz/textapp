3d608d3250bf630b993a74a959f82ee2260f154d
Himanshu Chandola <himanshuchandolaofficial@gmail.com>
2021-08-17 18:37:29 +0530

    changes in aboutText

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar';
 function App() {
   return (
   <>
-<Navbar title="TextApp" aboutText="About TextApp"/>
+<Navbar title="TextApp" aboutText="About"/>
   </>
   );
 }
