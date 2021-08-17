5c8355dae1575ee64bf0bef28d8845eafbf47fbf
Himanshu Chandola <himanshuchandolaofficial@gmail.com>
2021-08-17 16:51:13 +0530

    Updated

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+let name ="Himanshu";
+function App() {
+  return (
+  <>
+  <nav>
+    <li>Home</li>
+    <li>About</li>
+    <li>Contact</li>
+  </nav>
+  <div className="container">
+    <h1>Hello {name}</h1>
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates commodi rem dicta deserunt fuga, voluptate error vitae facilis ex, exercitationem aspernatur veritatis iusto unde quae accusamus debitis earum reprehenderit accusantium!
+
+    </p>
+  </div>
+  </>
+  );
+}
+
+export default App;
