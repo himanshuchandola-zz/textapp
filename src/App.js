74ab5b2482ce49006725d0af414f08c76eb45468
Himanshu Chandola <himanshuchandolaofficial@gmail.com>
2021-08-18 19:38:31 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React, { useState } from 'react';
+
 
 function App() {
+  const [mode, setMode] = useState('dark');
   return (
   <>
 {/* <Navbar title="TextApp by Himanshu Chandola" aboutText="About"/> */}
-<Navbar title="TextApp" />
+<Navbar title="TextApp" mode={mode} />
 <div className="container my-3">
 <TextForm heading="Enter The Text to analyze below"/>
 {/* <About/> */}
