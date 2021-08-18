7bb68e2cb3cb1db2df5dcbbf86ad79a9389755ff
Himanshu Chandola <himanshuchandolaofficial@gmail.com>
2021-08-18 20:42:08 +0530

    Dark Textfield implemented on Dark Mode

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -74,7 +74,8 @@ export default function TextForm(props) {
         <div ClassName="container" style={{color: props.mode==='dark'?'white':'black'}}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white'}} id="myBox" rows="8"></textarea>
+            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'black':'white',
+          color: props.mode==='dark'?'white':'black'}} id="myBox" rows="8"></textarea>
                 </div> 
                 <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to UpperCase</button>
                 <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to LowerCase</button>
