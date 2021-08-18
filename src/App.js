afc69b577c6340574d0d9cfb7551d7537dd13663
Himanshu Chandola <himanshuchandolaofficial@gmail.com>
2021-08-18 17:09:52 +0530

    Few Changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar';
-// import TextForm from './components/TextForm';
+import TextForm from './components/TextForm';
 
 function App() {
   return (
   <>
 <Navbar title="TextApp by Himanshu Chandola" aboutText="About"/>
 <div className="container my-3">
-{/* <TextForm heading="Enter The Text to analyze below"/> */}
-<About/>
+ <TextForm heading="Enter The Text to analyze below"/>
+{/* <About/> */}
 </div>
 
   </>
--- src/components/About.js
+++ src/components/About.js
@@ -3,6 +3,7 @@ import React from 'react'
 export default function About() {
     return (
         <div className="container">
+            <h1>About Us</h1>
             <div class="accordion" id="accordionExample">
                 <div class="accordion-item">
                     <h2 class="accordion-header" id="headingOne">
